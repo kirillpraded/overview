8b315bf43a15f319de052e549e34fedfaaf472d0
Kiryl Pradzed <kirillpraded@bk.ru>
2021-02-21 19:07:44 +0300

    second branch

--- src/main/java/by/praded/controller/MainController.java
+++ src/main/java/by/praded/controller/MainController.java
@@ -5,6 +5,8 @@ package by.praded.controller;
  * 21.02.2021
  */
 public class MainController {
+
+    public static final String DESCRIPTION = "q1w2e3";
     public static void main(String[] args) {
 
     }
